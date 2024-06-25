443c36e6e6bc3021819947b18a0da783c63b9f57
SandyL88 <SANDY66888866@OUTLOOK.COM>
2024-06-25 06:32:33 -0400

    Initial commit

---
+++ WelcomeToacme.cs
@@ -0,0 +1,10 @@
+using System;
+
+class WelcomeToAcme
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems\nRemember, we'r \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
