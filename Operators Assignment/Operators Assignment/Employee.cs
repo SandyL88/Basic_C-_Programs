2f9b04e47871297dd9fb367cc5e842877f46bf55
SandyL88 <SANDY66888866@OUTLOOK.COM>
2024-07-14 03:41:39 -0400

    module 6 operators assignment submit

---
+++ Operators Assignment/Operators Assignment/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    public class Employee
+    {
+        public int Id { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //== operation
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            //if emp1 and emp2 are null or emp1=emp2, Then true.
+            if (ReferenceEquals(emp1, emp2))
+            {
+                return true;
+            }
+
+            //if onle one is null, then its not equal.
+            if (ReferenceEquals(emp1, null) || ReferenceEquals(emp2, null))
+            {
+                return false;
+            }
+
+            return emp1.Id == emp2.Id;
+        }
+
+        //!= operator
+        public static bool operator != (Employee emp1, Employee emp2)
+        {
+            return !(emp2 == emp1);
+        }
+
+        //Equal
+        public override bool Equals(object obj)
+        {
+            if (obj is Employee)
+            {
+                Employee emp = (Employee) obj;
+                return this.Id ==emp.Id;
+            }
+            return false;
+        }
+        //rewrite GetHashCode
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+    }
+}
---
+++ Operators Assignment/Operators Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Operators_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee { Id = 1, FirstName = "Jessie", LastName = "Dodge" };
+            Employee emp2 = new Employee { Id = 2, FirstName = "Jane", LastName = "Doe" };
+
+            bool areEqual = emp1== emp2;
+            Console.WriteLine($"Employee 1 and Employee 2 are equal: {areEqual}");
+            
+            bool areNotEqual = emp2 != emp1;
+            Console.WriteLine($"Employee 1 and Employee 2 are not equal: {areNotEqual}");
+
+            Console.ReadLine();
+        }
+    }
+}
